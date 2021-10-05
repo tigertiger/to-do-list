3d7de07b7d64798bd78ea03ab6cb73c8f557a41f
Melissa <melissa.scmi@gmail.com>
2021-10-04 22:21:04 -0700

    complete entity integration

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -4,6 +4,18 @@ namespace ToDoList.Models
 {
   public class Category
   {
+    private static List<Category> _instances = new List<Category> {};
+    public string Name { get; set; }
+    public int Id { get; }
+    public List<Item> Items { get; set; }
+
+    public Category(string categoryName)
+    {
+      Name = categoryName;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Items = new List<Item>{};
+    }
 
     public static void ClearAll()
     {
@@ -25,17 +37,5 @@ namespace ToDoList.Models
       Items.Add(item);
     }
 
-    private static List<Category> _instances = new List<Category> {};
-    public string Name { get; set; }
-    public int Id { get; }
-    public List<Item> Items { get; set; }
-
-    public Category(string categoryName)
-    {
-      Name = categoryName;
-      _instances.Add(this);
-      Id = _instances.Count;
-      Items = new List<Item>{};
-    }
   }
 }
